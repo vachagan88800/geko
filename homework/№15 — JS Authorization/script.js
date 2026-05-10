e393ed2b4a37e06786dfe5912abe1c902b2458f7
vachagan88800 <vachagan88800@gmail.com>
2026-05-10 18:48:24 +0400

    ankap

---
+++ homework/№15 — JS Authorization/script.js
@@ -0,0 +1,5 @@
+const users = [
+  { username: "admin", password: "1234", isBlocked: false },
+  { username: "john", password: "qwerty", isBlocked: true },
+  { username: "anna", password: "pass", isBlocked: false }
+];
