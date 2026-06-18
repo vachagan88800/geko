ab1954110db75f27c914cb3e427a23e3fdb3ec42
Vachagan88800 <vachagan88800@gmail.com>
2026-06-18 21:14:11 +0400

    x and o

--- classroom/python/script.py
+++ classroom/python/script.py
@@ -1,10 +1,48 @@
-import random
+board = [" "] * 9
 
-secret = random.randint(1, 10)
+def print_board():
+    print(f"{board[0]} | {board[1]} | {board[2]}")
+    print("--+---+--")
+    print(f"{board[3]} | {board[4]} | {board[5]}")
+    print("--+---+--")
+    print(f"{board[6]} | {board[7]} | {board[8]}")
 
-guess = int(input("Enter a number: "))
+def check_winner():
+    wins = [
+        [0,1,2], [3,4,5], [6,7,8],
+        [0,3,6], [1,4,7], [2,5,8],
+        [0,4,8], [2,4,6]
+    ]
 
-while guess != secret:
-    guess = int(input("Try again: "))
+    for a, b, c in wins:
+        if board[a] == board[b] == board[c] != " ":
+            return board[a]
 
-print("You win!!!")
+    return None
+
+player = "X"
+
+while True:
+    print_board()
+
+    move = int(input(f"Player {player}, choose position (1-9): ")) - 1
+
+    if board[move] != " ":
+        print("Spot already taken!")
+        continue
+
+    board[move] = player
+
+    winner = check_winner()
+
+    if winner:
+        print_board()
+        print(f"Player {winner} wins!")
+        break
+
+    if " " not in board:
+        print_board()
+        print("It's a draw!")
+        break
+
+    player = "O" if player == "X" else "X"
